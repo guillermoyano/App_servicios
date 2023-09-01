8b4670de016059f7652d58ba99bd602c72980bbf
Guillermo Moyano <guillermoyano2002@gmail.com>
2023-09-01 02:21:32 -0300

    Primer Commit

---
+++ src/main/java/com/proyecto/EquipoUno/EquipoUnoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.EquipoUno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipoUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipoUnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/EquipoUno/EquipoUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.EquipoUno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipoUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
